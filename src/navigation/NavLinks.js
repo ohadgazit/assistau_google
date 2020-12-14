0a377f4ef9b4f671bd73d88adf4cd94f811d2753
David <dudioren92@gmail.com>
2020-12-14 18:40:06 +0200

    #Updated logo and design

--- src/navigation/NavLinks.js
+++ src/navigation/NavLinks.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import SignInPage from  "../pages/SignInPage";
 
 import './NavLinks.css';
 
@@ -15,10 +16,10 @@ const NavLinks = props => {
     {connected?<li>
       <NavLink to="/u1/places">איזור אישי</NavLink>
     </li>:<li>
-      <NavLink to="/u1/places">הרשם</NavLink>
+      <NavLink to="/UserForm">הרשם</NavLink>
     </li>}
     <li>
-      <NavLink to="/places/new">התחבר</NavLink>
+      <NavLink to="/SignIn">התחבר</NavLink>
     </li>
     {/*<li>
       <NavLink to="/auth">AUTHENTICATE</NavLink>
