6b6d705312688e30c7003ee0a1524486ad8cf919
ohadgazit <45396762+ohadgazit@users.noreply.github.com>
2021-02-19 18:17:20 +0200

    #Added new review mechanism

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -4,31 +4,12 @@ import { Link } from 'react-router-dom';
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import './MainNavigation.css';
-import Backdrop from '../Shared/Backdrop';
 
 const MainNavigation = props => {
-  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-
-  const openDrawerHandler = () => {
-    setDrawerIsOpen(true);
-  };
-
-  const closeDrawerHandler = () => {
-    setDrawerIsOpen(false);
-  };
 
   return (
     <React.Fragment>
-      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
-      
-
-
       <MainHeader>
-        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
-          <span />
-          <span />
-          <span />
-        </button>
         <h1 className="main-navigation__title">
           <Link to="/">AssisTAU</Link>
         </h1>
